c4bf137b3422c5509bd3d9e8181f2eda8b1c69c9
DanTsk <tsekhmastryuk@gmail.com>
2017-03-26 19:46:30 +0300

    Last message

--- src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
+++ src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
@@ -13,8 +13,14 @@ import com.google.api.server.spi.response.ConflictException;
 import com.google.api.server.spi.response.ForbiddenException;
 import com.google.api.server.spi.response.NotFoundException;
 import com.google.api.server.spi.response.UnauthorizedException;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
 import com.google.appengine.api.users.User;
 import com.google.devrel.training.conference.Constants;
+import com.google.devrel.training.conference.domain.Announcement;
 import com.google.devrel.training.conference.domain.Conference;
 import com.google.devrel.training.conference.domain.Profile;
 import com.google.devrel.training.conference.form.ConferenceForm;
@@ -184,7 +190,10 @@ public class ConferenceApi {
 		// TODO (Lesson 4)
 		// Save Conference and Profile Entities
 		ofy().save().entities(conference, profile).now();   
-       
+		final Queue queue = QueueFactory.getDefaultQueue();
+		queue.add(ofy().getTransaction(),TaskOptions.Builder.withUrl("/tasks/send_confirmation_email")
+                .param("email", profile.getMainEmail())
+                .param("conferenceInfo", conference.toString()));
          
 
          return conference;
@@ -480,4 +489,17 @@ public class ConferenceApi {
         }
 		return ofy().load().keys(keysToAttend).values(); // change this
 	}
+	
+	@ApiMethod(name = "getAnnouncement", path = "announcement", httpMethod = HttpMethod.GET)
+		public Announcement getAnnouncement() {
+	 		// TODO GET announcement from memcache by key and if it exist return it
+	 		MemcacheService memcacheService = MemcacheServiceFactory
+	 				.getMemcacheService();
+	 		Object message = memcacheService
+	 				.get(Constants.MEMCACHE_ANNOUNCEMENTS_KEY);
+	 		if (message != null) {
+	 		return new Announcement(message.toString());
+ 		}
+ 		return null;
+ 	}
 }
